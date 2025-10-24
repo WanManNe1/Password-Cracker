e0c3d2393fed1fe7affae96833f0da2133789b91
WanManNe1 <naletheeking@proton.me>
2025-10-24 12:48:54 +0300

    Password file generation script update

--- gen_argon2_passwd_file.py
+++ gen_argon2_passwd_file.py
@@ -26,32 +26,33 @@ ph = PasswordHasher(
 
 
 
-number_of_users = NUMBER_OF_USERS
-min_passwd_length = MIN_PASSWD_LENGTH
-max_passwd_length = MAX_PASSWD_LENGTH
+def main():
+	number_of_users, min_passwd_length, max_passwd_length = NUMBER_OF_USERS, MIN_PASSWD_LENGTH, MAX_PASSWD_LENGTH
+
+	try: number_of_users = int(input(f'Enter the NUMBER OF USERS:  '))
+	except Exception as e: print(f'{e}..... USING DEFAULT VALUE {NUMBER_OF_USERS}')
+
+	try: min_passwd_length = int(input(f'Enter the MINIMUM PASSWORD LENGTH:  '))
+	except Exception as e: print(f'{e}..... USING DEFAULT VALUE {MIN_PASSWD_LENGTH}')
+
+	try: max_passwd_length = int(input(f'Enter the MAXIMUM PASSWORD LENGTH:  '))
+	except Exception as e: print(f'{e}..... USING DEFAULT VALUE {MAX_PASSWD_LENGTH}')
+
+	with open(file=PASSWD_FILE_NAME, newline='', mode='w', encoding='utf-8') as passwd_file:
+		writer = csv.writer(passwd_file)
+
+		for i in range(number_of_users):
+			username = f'{UNAME_PREFIX}{i+1:02}'
+			passwd = ''.join(secrets.choice(PERMITTED_ALPHABET) for i in range(random.randint(min_passwd_length, max_passwd_length)))
+			hashed_passwd = None
+
+			try: hashed_passwd = ph.hash(password=passwd)
+			except HashingError as hashing_err: print(hashing_err)
+			except Exception as e: print(e)
+
+			writer.writerow([username, passwd, hashed_passwd])
 
 
-try: number_of_users = int(input(f'Enter the NUMBER OF USERS:  '))
-except Exception as e: print(f'{e} \n USING DEFAULT VALUE {NUMBER_OF_USERS}')
 
-try: min_passwd_length = int(input(f'Enter the MINIMUM PASSWORD LENGTH:  '))
-except Exception as e: print(f'{e} \n USING DEFAULT VALUE {MAX_PASSWD_LENGTH}')
-
-try: max_passwd_length = int(input(f'Enter the MAXIMUM PASSWORD LENGTH:  '))
-except Exception as e: print(f'{e} \n USING DEFAULT VALUE {MIN_PASSWD_LENGTH}')
-
-
-
-with open(file=PASSWD_FILE_NAME, newline='', mode='w', encoding='utf-8') as passwd_file:
-	writer = csv.writer(passwd_file)
-
-	for i in range(number_of_users):
-		username = f'{UNAME_PREFIX}{i+1:02}'
-		passwd = ''.join(secrets.choice(PERMITTED_ALPHABET) for i in range(random.randint(min_passwd_length, max_passwd_length)))
-		hashed_passwd = None
-
-		try: hashed_passwd = ph.hash(password=passwd)
-		except HashingError as hashing_err: print(hashing_err)
-		except Exception as e: print(e)
-
-		writer.writerow([username, passwd, hashed_passwd])
+if __name__ == '__main__':
+	main()
